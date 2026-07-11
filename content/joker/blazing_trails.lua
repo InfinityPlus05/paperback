441de9308008c6e75587c90889e987e7bf5689f6
srockw <136873475+srockw@users.noreply.github.com>
2026-07-11 16:55:51 -0300

    (Blazing Trails) actually blueprint compatible now

--- content/joker/blazing_trails.lua
+++ content/joker/blazing_trails.lua
@@ -32,10 +32,23 @@ SMODS.Joker {
   end,
 
   calculate = function(self, card, context)
-    if context.pre_discard and #context.full_hand == card.ability.extra.required_cards and not context.blueprint then
-      local to_destroy = context.full_hand[#context.full_hand]
-      to_destroy:juice_up()
-      SMODS.destroy_cards(to_destroy)
+    if context.pre_discard and #context.full_hand == card.ability.extra.required_cards then
+      local to_destroy = nil
+
+      -- Collect the rightmost card already not destroyed by blazing trails
+      for _, v in ipairs(context.full_hand) do
+        if not v.paperback_blazing_destroyed then
+          to_destroy = v
+        end
+      end
+
+      if to_destroy then
+        to_destroy.paperback_blazing_destroyed = true
+        to_destroy:juice_up()
+        SMODS.destroy_cards(to_destroy, { delay = 0.1 })
+
+        return nil, true
+      end
     end
   end
 }
